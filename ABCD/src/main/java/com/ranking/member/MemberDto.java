4e63800d93db4834d6bf3eac41bbab717659c394
IOTspecialist <dozo1212@naver.com>
2017-12-03 17:02:05 +0900

    memberDto만들음,mybatis설정파일추가, data설정파일에 아파치데이터랑 스프링데이터 추가해놨음

---
+++ ABCD/src/main/java/com/ranking/member/MemberDto.java
@@ -0,0 +1,44 @@
+package com.ranking.member;
+
+public class MemberDto {
+
+	private int m_num;
+	private String m_id;
+	private String m_password;
+	private int m_point;
+
+	
+	
+	public int getM_num() {
+		return m_num;
+	}
+
+	public void setM_num(int m_num) {
+		this.m_num = m_num;
+	}
+
+	public String getM_id() {
+		return m_id;
+	}
+
+	public void setM_id(String m_id) {
+		this.m_id = m_id;
+	}
+
+	public String getM_password() {
+		return m_password;
+	}
+
+	public void setM_password(String m_password) {
+		this.m_password = m_password;
+	}
+
+	public int getM_point() {
+		return m_point;
+	}
+
+	public void setM_point(int m_point) {
+		this.m_point = m_point;
+	}
+
+}
