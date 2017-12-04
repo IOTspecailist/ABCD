8fdb436c81387cfd70f936d31005507ecf54cf24
IOTspecialist <dozo1212@naver.com>
2017-12-04 19:30:45 +0900

    MemberMapper인터페이스를 이용해 DB정보, MemberMapper.xml파일의 쿼리 정보등을 컨테이너로부터 주입받는다. @Autowired로 했음

--- ABCD/src/main/java/com/ranking/member/MemberDaoImpl.java
+++ ABCD/src/main/java/com/ranking/member/MemberDaoImpl.java
@@ -2,11 +2,18 @@ package com.ranking.member;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.ranking.mapper.member.MemberMapper;
 
 @Repository	//DAO에 붙이는 어노테이션이다. 프로그램상에선 '이게 붙은게 DAO다'라고 알려주는 역할인 것 같다. application-config에서 스캔한다
 public class MemberDaoImpl implements MemberDao {
 
+	
+	@Autowired
+	MemberMapper memberMapper;
+	
 	@Override
 	public int insertMember(MemberDto member) {
 		// TODO Auto-generated method stub
